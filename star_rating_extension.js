6a9b2ab3b88f5893fa1df110ab4416278378e16a
Chris-Terminator <chriscurry.1213@gmail.com>
2025-07-08 15:39:16 -0500

    Update star_rating_extension.js

--- star_rating_extension.js
+++ star_rating_extension.js
@@ -6,7 +6,7 @@ const StarFeedbackExtension = {
   render: ({ trace, element }) => {
     // SVG for star icon
     const SVG_Star = `
-      <svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
       </svg>
     `
@@ -17,10 +17,10 @@ const StarFeedbackExtension = {
             .vfrc-feedback {
                 display: flex;
                 align-items: center;
-                justify-content: space-between;
+                justify-content: center;
             }
             .vfrc-feedback--description {
-                font-size: 14px;
+                font-size: 0.8em;
                 color: #1a1e23;
                 pointer-events: none;
                 font-family: 'UCity Pro', sans-serif;
@@ -62,7 +62,6 @@ const StarFeedbackExtension = {
             }
           </style>
           <div class="vfrc-feedback">
-            <div class="vfrc-feedback--description">Was this helpful?</div>
             <div class="vfrc-feedback--buttons">
               <button class="vfrc-feedback--button" data-feedback="1">${SVG_Star}</button>
               <button class="vfrc-feedback--button" data-feedback="2">${SVG_Star}</button>
